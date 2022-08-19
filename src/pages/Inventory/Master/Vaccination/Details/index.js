9bab153af87eae8581bdd1917ca10027f4d2a493
sun_wang <sun_wang@medinno.onmicrosoft.com>
2022-08-19 16:04:55 +0800

    #24084

--- src/pages/Inventory/Master/Vaccination/Details/index.js
+++ src/pages/Inventory/Master/Vaccination/Details/index.js
@@ -196,6 +196,7 @@ export default compose(
         ...returnValue,
         schemes,
         clinicSettings: clinicSettings.settings,
+        dispensingQuantity: returnValue.dispensingQuantity || null,
       }
     },
 
@@ -250,6 +251,9 @@ export default compose(
       excessThreshold: Yup.number()
         .min(0, 'Excess Threshold must between 0 and 999,999.9')
         .max(999999.9, 'Excess Threshold must between 0 and 999,999.9'),
+      dispensingQuantity: Yup.number()
+        .integer('Must be an integer')
+        .min(1, 'Min. value is 1'),
     }),
     handleSubmit: (values, { props, resetForm }) => {
       const { dispatch, history } = props
